49ed0db5285c914c2f191e73fa9583b0ee4039a7
Maurizio Hostettler <maurizio.hostettler@gmail.com>
2022-05-12 12:38:20 +0200

    refactor to local main

--- src/zen/zen_of_python.py
+++ src/zen/zen_of_python.py
@@ -1,10 +1,14 @@
-import this
-
-
 def print_zen():
     """
     Instead of printing each row in this class we live the zen of python by importing "this" which is an easter-egg
     that already does exactly what we want :-) which is - the zen of python
     """
+    import this
+
     if this:
         pass
+
+
+if __name__ == '__main__':
+    print_zen()
+
