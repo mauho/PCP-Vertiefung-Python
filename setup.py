26dd545f9042655f8f1c7e8b9d491439555c5178
Maurizio Hostettler <maurizio.hostettler@gmail.com>
2022-05-13 10:56:04 +0200

    add requests package to requirements and setup

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
+
 from setuptools import setup
 
 setup(name='pcp-deep-dive',
       version='1.0',
       py_modules=['pcp'],
-      install_requires=[
+      install_requires=['requests',
       ],
       # ... more options/metadata
       )
